a3a447a15c50b8236c501c712f9a6d6b04b171b4
Anthony Picciano <picciano1@Anthonys-MacBook-Pro.local>
2015-01-21 14:05:19 -0700

    Initial Commit

---
+++ Hunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hunch
+//
+//  Created by Anthony Picciano on 1/21/15.
+//  Copyright (c) 2015 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hunch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hunch
+//
+//  Created by Anthony Picciano on 1/21/15.
+//  Copyright (c) 2015 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
